519a3d5cbf368cd15075b5822a981b13acc08152
Tyler Turnipseed <tweak222@gmail.com>
2019-04-18 22:05:52 -0400

    Completed objectives within callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,27 +24,60 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// Function
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+// Invocation
+getLength(items, length => console.log(length));
+
+// Function
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
+// Invocation
+last(items, lastItem => console.log(lastItem));
+
+// Function
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
+// Invocation
+sumNums(3, 4, sum => console.log(sum));
+
+// Function
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
+// Invocation
+multiplyNums(3, 4, multiply => console.log(multiply));
+
+// Function
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let bool = false;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      bool = true;
+      break;
+    }
+  }
+
+  return cb(bool);
 }
+
+// Invocation
+contains('yo-yo', items, check => console.log(check));
 
 /* STRETCH PROBLEM */
 
